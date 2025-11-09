ba07753a482b6b5f8bb837fa2b3597797b8a6d89
Komla Sanussi <your.kamilousanussi@gmail.com>
2025-11-09 22:01:40 +0000

    Add flex styling to Navbar for ALX inline styling check

--- my-company/src/components/Navbar.jsx
+++ my-company/src/components/Navbar.jsx
@@ -2,13 +2,21 @@ import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
-    <nav style={{ backgroundColor: "#333", padding: "10px" }}>
-      <Link to="/" style={{ color: "white", marginRight: "15px" }}>Home</Link>
-      <Link to="/about" style={{ color: "white", marginRight: "15px" }}>About</Link>
-      <Link to="/services" style={{ color: "white", marginRight: "15px" }}>Services</Link>
+    <nav
+      style={{
+        backgroundColor: "#333",
+        padding: "10px",
+        display: "flex",           // added
+        justifyContent: "space-around" // added
+      }}
+    >
+      <Link to="/" style={{ color: "white" }}>Home</Link>
+      <Link to="/about" style={{ color: "white" }}>About</Link>
+      <Link to="/services" style={{ color: "white" }}>Services</Link>
       <Link to="/contact" style={{ color: "white" }}>Contact</Link>
     </nav>
   );
 }
 
 export default Navbar;
+
