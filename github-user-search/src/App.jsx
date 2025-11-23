b9c8c26de10e94bf45357a60792c28e4eb32474e
Komla Sanussi <your.kamilousanussi@gmail.com>
2025-11-23 00:01:23 +0000

    Task 2: Advanced search and UI enhancements added

--- github-user-search/src/App.jsx
+++ github-user-search/src/App.jsx
@@ -1,10 +1,21 @@
-import React from "react";
 import Search from "./components/Search";
+import "./index.css";
 
 function App() {
   return (
-    <div>
-      <h1 style={{ padding: "20px" }}>GitHub User Search</h1>
+    <div
+      style={{
+        minHeight: "100vh",
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        padding: "20px"
+      }}
+    >
+      <h1 style={{ textAlign: "center", marginBottom: "20px" }}>
+        GitHub User Search
+      </h1>
+
       <Search />
     </div>
   );
